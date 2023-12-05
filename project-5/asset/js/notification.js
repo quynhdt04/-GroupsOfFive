7a25859f256ad98e275120ceb6c5e76613d6f148
Đinh Thị Quỳnh <quynhdinh704@gmail.com>
2023-12-05 19:55:23 +0700

    complete

---
+++ project-5/asset/js/notification.js
@@ -0,0 +1,16 @@
+function notification() {
+    event.defaultPrevented;
+    alert("Bạn chưa đăng nhập");
+}
+
+function notifi(){
+    alert("Đã thêm sản phẩm vào giỏ hàng");
+}
+
+function sign(){
+    alert("Đăng ký thành công");
+}
+
+function save() {
+    alert("Đã lưu thông tin");
+}
